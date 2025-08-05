a847c821089cfb51df28685b9d791679a98f0789
Abril Dacuez <dacuezabrilanahi@gmail.com>
2025-08-05 10:45:34 -0300

    correcciones (compila pero da error de acceso al agregar a la lista)

--- Tp_inicial/eventoslista.pas
+++ Tp_inicial/eventoslista.pas
@@ -1,5 +1,17 @@
 program eventoslista;
-
+uses unit_lista;
+var
+  L:TListaEventos;
+  E:TEvento;
 begin
-
-end;
+  CREARLISTA(L);
+  pedir_datos(E);
+  AGREGAR(L,E);
+  MUESTRA_LISTA(L);
+ 
+//   BUSCAR_titulo(L,'Cumpleaños',ENC);
+//   if ENC then
+//     writeln('Evento encontrado')
+//   else
+//     writeln('Evento no encontrado');
+end.
--- Tp_inicial/unit_lista.pas
+++ Tp_inicial/unit_lista.pas
@@ -1,18 +1,21 @@
-unit uniTListaEventos;
+unit unit_lista;
 
 interface
 
+
 type
+  t_evento = (cumple, reunion, otro); // <-- Declarar primero
+
   TEvento = record
-    id:integer;
-    fechainicio:string[10];
-    fechafin:string[10];
-    horainicio:string;
-    horafin:string;
-    ubicacion:string;
+    id: integer;
+    fechainicio: string[10];
+    fechafin: string[10];
+    horainicio: string;
+    horafin: string;
+    ubicacion: string;
     titulo: string;
-    descripcion:string;
-    t_evento: (cumpleaños,reunion,otro);
+    descripcion: string;
+    t_evento: t_evento; // <-- Usar el tipo global aquí
   end;
 
   TListaEventos = record
@@ -21,18 +24,16 @@ type
   end;
 
 PROCEDURE CREARLISTA (VAR L:TListaEventos);
-PROCEDURE ELIMINARLISTA (VAR L:TListaEventos; buscado: integer; VAR X:TEvento;);
+PROCEDURE ELIMINAREVENTO (VAR L:TListaEventos; buscado: integer; VAR X:TEvento);
 FUNCTION LISTA_LLENA (VAR L:TListaEventos): BOOLEAN;
 FUNCTION LISTA_VACIA (VAR L:TListaEventos): BOOLEAN;
 PROCEDURE AGREGAR (VAR L:TListaEventos; X:TEvento);
+procedure pedir_datos (var E:TEvento);
 procedure Muestra_datos (E:TEvento);
-procedure fin (L:TListaEventos):boolean;
-PROCEDURE MUESTRA_LISTA(L:TListaEventos); 
-procedure recuperar (L:TListaEventos; var E:TEvento; i:integer);
+PROCEDURE MUESTRA_LISTA(L:TListaEventos); //LISTADO
 PROCEDURE BUSCAR_titulo (L:TListaEventos; BUSCADO:string; VAR ENC:BOOLEAN); 
-PROCEDURE BUSCAR_entre_fechas(L:TListaEventos; fecha1,fecha2:string[10]; VAR ENC:BOOLEAN); 
+PROCEDURE BUSCAR_entre_fechas(L:TListaEventos; fecha1,fecha2:string; VAR ENC:BOOLEAN); //SI BUSCA POR CAMPO CLAVE O TIPO SIMPLE EN INFO
 PROCEDURE BUSCAR_tipo (L:TListaEventos; BUSCADO:STRING; VAR ENC:BOOLEAN); 
-
 
 implementation
 
@@ -41,41 +42,46 @@ var i:integer;
 begin
 for i:=1 to 200 do
   begin
-    SetLength(L, i);
-    L[i-1].id := 0;
-    L[i-1].Tfecha.fechainicio := '';
-    L[i-1].Tfecha.fechafin := '';
-    L[i-1].Thora.horainicio := '';
-    L[i-1].Thora.horafin := '';
-    L[i-1].ubicacion := '';
-    L[i-1].titulo := '';
-    L[i-1].descripcion := '';
-    L[i-1].t_evento := 'otro'; 
-  end;
+    L.eventos[i].id := i;
+    L.eventos[i].fechainicio := '';
+    L.eventos[i].fechafin := '';
+    L.eventos[i].horainicio := '';
+    L.eventos[i].horafin := '';
+    L.eventos[i].ubicacion := '';
+    L.eventos[i].titulo := '';
+    L.eventos[i].descripcion := '';
+    L.eventos[i].t_evento := otro; 
+    //inc(i);
+     end;
+L.cant := 0; // Inicializar la cantidad de eventos a 0
+end;
 
-PROCEDURE ELIMINARLISTA (VAR L:TListaEventos; buscado: integer; VAR X:TEvento;);
+PROCEDURE ELIMINAREVENTO (VAR L:TListaEventos; buscado: integer; VAR X:TEvento);
 VAR
 i:integer;
 BEGIN
-FOR i:=0 TO L.cant DO
-BEGIN
+i:=1;
 IF L.eventos[i].id = buscado THEN
-    BEGIN
-    X:= L.eventos[i];
-    for i:=i to L.eventos[i].id=0 do
-    begin
-    L.eventos[i].Tfecha.fechainicio := L.eventos[i+1].Tfecha.fechainicio;
-    L.eventos[i].Tfecha.fechafin := L.eventos[i+1].Tfecha.fechafin;
-    L.eventos[i].Thora.horainicio := L.eventos[i+1].Thora.horainicio;
-    L.eventos[i].Thora.horafin := L.eventos[i+1].Thora.horafin;
-    L.eventos[i].ubicacion := L.eventos[i+1].ubicacion;
-    L.eventos[i].titulo := L.eventos[i+1].titulo;
-    L.eventos[i].descripcion := L.eventos[i+1].descripcion;
-    end;
-    dec(L.cant); // Reducir el tamaño del array
+      BEGIN
+      X:= L.eventos[i];
+        for i:=i to L.cant do
+        begin
+        L.eventos[i].fechainicio := L.eventos[i+1].fechainicio;
+        L.eventos[i].fechafin := L.eventos[i+1].fechafin;
+        L.eventos[i].horainicio := L.eventos[i+1].horainicio;
+        L.eventos[i].horafin := L.eventos[i+1].horafin;
+        L.eventos[i].ubicacion := L.eventos[i+1].ubicacion;
+        L.eventos[i].titulo := L.eventos[i+1].titulo;
+        L.eventos[i].descripcion := L.eventos[i+1].descripcion;
+        end;
+      dec(L.cant); // Reducir el tamaño del array
     end
     ELSE
     inc(i);
+    ELIMINAREVENTO(L,buscado,X);
+    if i=201 then
+      begin
+        writeln('Evento no encontrado');
     end;
 end;
 
@@ -94,29 +100,50 @@ begin
 if not lista_llena(L) then
   begin
     L.eventos[L.cant] := X; // Agregar el nuevo evento al final del array
-    L.eventos[L.cant].id := L.cant+1;
+    L.eventos[L.cant].id := L.cant+1; 
     inc(L.cant); // Incrementar la cantidad de eventos
   end;
 end;
 
+procedure pedir_datos (var E:TEvento);
+var tipo: byte;
+begin
+writeln('Ingrese titulo del evento:');
+readln(E.titulo);
+writeln('Ingrese descripcion del evento:');
+readln(E.descripcion); 
+writeln('Ingrese fecha de inicio (dd/mm/yyyy):');
+readln(E.fechainicio);
+writeln('Ingrese fecha de fin (dd/mm/yyyy):');
+readln(E.fechafin);
+writeln('Ingrese hora de inicio (hh:mm):');
+readln(E.horainicio);
+writeln('Ingrese hora de fin (hh:mm):');
+readln(E.horafin);
+writeln('Ingrese ubicacion del evento:');
+readln(E.ubicacion);
+writeln('Ingrese tipo de evento (1: cumple, 2: reunion, 3: otro):');
+readln(tipo);
+case tipo of
+  1: E.t_evento := cumple;
+  2: E.t_evento := reunion;
+  else E.t_evento := otro;
+end;
+end;
 
 procedure Muestra_datos (E:TEvento);
 begin
 writeln('ID: ', E.id);
-writeln('Fecha Inicio: ', E.Tfecha.fechainicio);
-writeln('Fecha Fin: ', E.Tfecha.fechafin);
-writeln('Hora Inicio: ', E.Thora.horainicio);
-writeln('Hora Fin: ', E.Thora.horafin);
+writeln('Fecha Inicio: ', E.fechainicio);
+writeln('Fecha Fin: ', E.fechafin);
+writeln('Hora Inicio: ', E.horainicio);
+writeln('Hora Fin: ', E.horafin);
 writeln('Ubicación: ', E.ubicacion);
 writeln('Título: ', E.titulo);
 writeln('Descripción: ', E.descripcion);
 writeln('Tipo de Evento: ', E.t_evento);
 end; 
 
-procedure fin (L:TListaEventos):boolean;
-begin
-fin:= L.cant=200;
-end;
 
 PROCEDURE MUESTRA_LISTA(L:TListaEventos); //LISTADO
 VAR
@@ -124,71 +151,77 @@ E:TEvento;
 i:integer;
 BEGIN
 i:=1;
-WHILE NOT FIN(L) DO
+WHILE (i <= L.cant) DO
     BEGIN
-    RECUPERAR(L,E,i);
+    E := L.eventos[i];
     MUESTRA_DATOS(E);
     i:=i+1;
     END;
 end;
 
-procedure recuperar (L:TListaEventos; var E:TEvento; i:integer);
-begin
-E:=L.eventos[i]; // Recuperar el último evento agregado
-end;
 
-PROCEDURE BUSCAR_titulo (L:TListaEventos; BUSCADO:string; VAR ENC:BOOLEAN); //SI BUSCA POR CAMPO CLAVE O TIPO SIMPLE EN INFO
+PROCEDURE BUSCAR_titulo (L:TListaEventos; BUSCADO:string; VAR ENC:BOOLEAN); 
 VAR
-E:TEvento; POS:BYTE; i:integer;
+E:TEvento; aux:BYTE; i:integer;
 BEGIN
-i:=1; POS:=0; ENC:=FALSE
-WHILE NOT FIN(L) AND (POS=0) DO
-    BEGIN
-    for i:=1 to L.cant do begin
-    RECUPERAR(L[i].evento,E,i);
-    IF BUSCADO in E.titulo then pos:=1;
-    Muestra_datos(E); end
-    ELSE inc(i);
-    end;
+i:=1;  ENC:=FALSE;
+WHILE (i <= L.cant) AND enc=false DO
+      BEGIN
+      for i:=1 to L.cant do 
+          begin
+          E:= L.eventos[i];
+          aux:= Pos(BUSCADO, E.titulo);
+          IF aux > 0 then
+            begin
+              enc:= true;
+              Muestra_datos(E);
+            end;
+        end;
+      end;
 END;
 
-PROCEDURE BUSCAR_entre_fechas(L:TListaEventos; fecha1,fecha2:string[10]; VAR ENC:BOOLEAN); //SI BUSCA POR CAMPO CLAVE O TIPO SIMPLE EN INFO
+PROCEDURE BUSCAR_entre_fechas(L:TListaEventos; fecha1,fecha2:string; VAR ENC:BOOLEAN); //SI BUSCA POR CAMPO CLAVE O TIPO SIMPLE EN INFO
 VAR
 E:TEvento; POS:BYTE; i:integer;
 BEGIN
-i:=1; POS:=0; ENC:=FALSE
-WHILE NOT FIN(L) AND (POS=0) DO
+i:=1;  ENC:=FALSE;
+WHILE (i <= L.cant) AND enc=false DO
 BEGIN
 for i:=1 to L.cant do
     begin
-    RECUPERAR(L,E,i);
-    IF (E.Tfecha.fechainicio >= fecha1) AND (E.Tfecha.fechafin <= fecha2) THEN
-        BEGIN
+    E:= L.eventos[i];
+    IF (E.fechainicio >= fecha1) AND (E.fechafin <= fecha2) THEN
         Muestra_datos(E);
-            END ELSE
-            inc(i);
+            // END ELSE
+            // inc(i);
             end;
     END;
 end;
 
-PROCEDURE BUSCAR_tipo (L:TListaEventos; BUSCADO:STRING; VAR ENC:BOOLEAN); 
+PROCEDURE BUSCAR_tipo (L:TListaEventos; BUSCADO:STRING; VAR ENC:BOOLEAN);
 VAR
-E:TEvento; POS:BYTE;i:integer;
+  E: TEvento;
+  i: integer;
+  tipo: t_evento; // Usar el tipo global
 BEGIN
-i:=1; POS:=0; ENC:=FALSE;
-WHILE NOT FIN(L) AND (POS=0) DO
-    BEGIN
-    for i:=1 to L.cant do
-        begin
-        RECUPERAR(L,E,i);
-        IF E.T_evento = BUSCADO THEN 
-            begin
-            MUESTRA_DATOS(E);
-            end
-            ELSE inc(i);
-            END;
-        end;
+  ENC := FALSE;
+  // Convertir string a tipo enumerado
+  if LowerCase(BUSCADO) = 'cumple' then
+    tipo := cumple
+  else if LowerCase(BUSCADO) = 'reunion' then
+    tipo := reunion
+  else
+    tipo := otro;
+
+  for i := 1 to L.cant do
+  begin
+    E := L.eventos[i];
+    if E.t_evento = tipo then
+    begin
+      ENC := TRUE;
+      Muestra_datos(E);
     end;
+  end;
 END;
 
 
