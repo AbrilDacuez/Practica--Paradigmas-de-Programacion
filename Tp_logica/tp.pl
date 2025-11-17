578bea3307c10127dc2ca631fc67fbf71ef8b449
mario <mario.caballero.948@gmail.com>
2025-11-17 17:13:41 -0300

    ejercicios de nivel 2

--- Tp_logica/tp.pl
+++ Tp_logica/tp.pl
@@ -1,8 +1,11 @@
+% Nivel 2
+% Ej 5
 cant([], 0).
 cant([_|R], N) :-
 	cant(R, N1),
 	N is N1 + 1.
 
+% Ej 16
 minimo([P], P).
 minimo([P|R], P) :-
 	minimo(R, M),
@@ -29,3 +32,37 @@ minimo([P|R], M) :-
 % probarImpares(N, I, Lim, R) :-
 % 	In is I + 2,
 % 	probarImpares(N, In, Lim, R).
+
+% Ej 8
+eliminarIesimo([], _, []).
+eliminarIesimo([_|X], N, Res) :-
+	N = 1,
+	Res = X.
+eliminarIesimo([L|XS], N, [L|Res]) :-
+	N > 1,
+	N1 is N - 1,
+	eliminarIesimo(XS, N1, Res).
+
+% Ej 6
+sumaLista([], 0).
+sumaLista([P|R], S) :-
+	sumaLista(R, S1),
+	S is P + S1.
+
+% Ej 10
+mediaLista([], 0).
+mediaLista(L, M) :-
+	cant(L, C),
+	sumaLista(L, S),
+	M is S/C.
+
+% Ej 11
+agregar(L, X, P, Res):-
+	P = 1,
+	Res = [X|L].
+agregar([L|R], X, P, [L|Res]) :-
+	P > 1,
+	P1 is P - 1,
+	agregar(R, X, P1, Res).
+
+% -------------------------------------------
