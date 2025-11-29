7775c4f8ef6172bbf720db4c3135d44321955f53
Mario Caballero <mario.caballero.948@gmail.com>
2025-11-29 13:21:06 -0300

    feat: se agrega parcial 2/12/2024

--- PracticaParcial_LogicaFuncional/funcional.exs
+++ PracticaParcial_LogicaFuncional/funcional.exs
@@ -1,4 +1,5 @@
 defmodule Parcial do
+  # Parcial 02/12/2024
   # ejercicio 1: escriba una funcion que reciba una lista sin sublistas L y un numero N y devuelva una lista con los elementos de L
   # que estan en las posiciones multiplos de N.
 
@@ -30,7 +31,72 @@ defmodule Parcial do
       true -> mult(xs, n)
     end
   end
+
+  # Ejercicio 2: Escriba una funcion que, dada una lista con sublistas L, devuelva una lista donde el iésimo elemento representa la sumatoria de los números contenidos en nivel i de profundidad de L. Si no hay elementos numéricos en un nivel específico, ese nivel tendrá valor 0 en la lista resultante.
+  # Ej L=[1,[2,3],[[4,5],6],[7,[8,[9]]]] -> Res [1,18,17,9]
+  # Nivel 1: 1
+  # Nivel 2: 2+3+6+7 = 18
+  # Nivel 3: 4+5+8 = 17
+  # Nivel 4: 9
+
+  def sumNivel([]), do: 0
+  # def sumNivel([x]), do: x
+  def sumNivel([x|xs]) do
+    cond do
+      is_list(x) -> sumNivel(xs)
+      true -> x + sumNivel(xs)
+    end
+  end
+
+  #L = [1,[2,3],[[4,5],6],[7,[8,[9]]]]
+  #L[0] = 1
+  #L[1] = [2,3]
+  #L[2] = [[4,5],6]
+
+  def elmNivel([]), do: []
+  def elmNivel([x|xs]) do
+    cond do
+      is_list(x) -> [x | elmNivel(xs)]
+      true -> elmNivel(xs)
+    end
+  end
+
+  def linNivel([]), do: []
+  def linNivel([x|xs]) do
+    cond do
+      is_list(x) -> x ++ linNivel(xs)
+      true -> [x | linNivel(xs)]
+    end
+  end
+
+  def lisSumIes([]), do: []
+  def lisSumIes(l) do
+     [sumNivel(l) | lisSumIes(linNivel(elmNivel(l)))]
+  end
+
+  # ----------------------------------
+  # Parcial
+#  esriba la función (predicado) que tome como entrada una lista con sublistas de un sólo nivel y una posición P
+#  y elimine de cada sublista los elementos que están en la posición P. [40 puntos]
+#  Lista: ((3 8 4 2 3) (1 6 3 9 8) (12 15))
+# P=3 y Resultado: ((3 8 2 3) (1 6 9 8) (12 15))
+
+  def eliminarX([],_x), do: []
+  def eliminarX([p | r], x) do
+    cond do
+      x == 1 -> r
+      true -> [p | eliminarX(r, x-1)]
+    end
+  end
+  def eliminarsegunX([],_x), do: []
+  def eliminarsegunX([p|r],x), do: [eliminarX(p,x) | eliminarsegunX(r,x)]
+
 end
 
-IO.inspect(Parcial.mult([10, 20, 30, 40, 50, 60, 70, 80], 3))
-IO.inspect(Parcial.mult([4, 5, 9, 6, 12, 8, 6, 5, 4, 3, 2, 3, 5, 10], 3))
+IO.inspect(Parcial.lisSumIes([[2,3],[[4,5],6],[7,[8,[9]]],3]),charlists: true)
+IO.inspect(Parcial.lisSumIes([1,[2,3],[[4,5],6],[7,[8,[9]]],3]),charlists: true)
+IO.inspect(Parcial.lisSumIes([1,[2,3],[[4,5],6],[7,[8,[9]]]]),charlists: true)
+IO.inspect(Parcial.lisSumIes([[2,3],[[4,5],6],2,[7,[8,[9]]]]),charlists: true)
+# IO.inspect(Parcial.linNivel([[2,3],[[4,5],6],[7,[8,[9]]]]),charlists: true)
+# IO.inspect(Parcial.linNivel([[4, 5],[8, [9]]]),charlists: true)
+# IO.inspect(Parcial.mult([4, 5, 9, 6, 12, 8, 6, 5, 4, 3, 2, 3, 5, 10], 3))
--- PracticaParcial_LogicaFuncional/logica.pl
+++ PracticaParcial_LogicaFuncional/logica.pl
@@ -1,3 +1,6 @@
+  % Parcial 02/12/2024
+  % ejercicio 1: escriba una funcion que reciba una lista sin sublistas L y un numero N y devuelva una lista con los elementos de L
+  % que estan en las posiciones multiplos de N.
 cant([], 0).
 cant([_|R], Res) :-
 	cant(R, Res2),
@@ -16,3 +19,58 @@ mult([L|R], N, Res) :-
 	X is C mod N,
 	X \= 0,
 	mult(R, N, Res).
+
+  % Ejercicio 2: Escriba una funcion que, dada una lista con sublistas L, devuelva una lista donde el iésimo elemento representa la sumatoria de los números contenidos en nivel i de profundidad de L. Si no hay elementos numéricos en un nivel específico, ese nivel tendrá valor 0 en la lista resultante.
+  %  Ej L=[1,[2,3],[[4,5],6],[7,[8,[9]]]] -> Res [1,18,17,9]
+  %  Nivel 1: 1
+  %  Nivel 2: 2+3+6+7 = 18
+  %  Nivel 3: 4+5+8 = 17
+  %  Nivel 4: 9
+
+sumNivel([], 0).
+sumNivel([P|R], Res) :-
+	is_list(P),
+	sumNivel(R, Res).
+sumNivel([P|R], Res) :-
+	sumNivel(R, Res2),
+	Res is P + Res2.
+	
+elmNivel([], []).
+elmNivel([P|R], [P|Res2]) :-
+	is_list(P),
+	elmNivel(R, Res2).
+elmNivel([_|R], Res) :-
+	elmNivel(R, Res).
+
+linNivel([], []).
+linNivel([P|R], Res) :-
+	is_list(P),
+	linNivel(R, Res2),
+	append(P, Res2, Res).
+linNivel([P|R], [P|Res2]) :-
+	linNivel(R, Res2).
+
+lisSumIes([], []).
+lisSumIes(L, [S|Res2]) :-
+	sumNivel(L, S),
+	elmNivel(L, N),
+	linNivel(N, Lin),
+	lisSumIes(Lin, Res2).
+
+
+% --------------------------------------
+	% Parcial
+	% esriba la función (predicado) que tome como entrada una lista con sublistas de un sólo nivel y una posición P
+	% y elimine de cada sublista los elementos que están en la posición P. [40 puntos]
+	% Lista: ((3 8 4 2 3) (1 6 3 9 8) (12 15))
+	% P=3 y Resultado: ((3 8 2 3) (1 6 9 8) (12 15))
+	eliminarX([], _X, []).
+eliminarX([_X|R], 1, R).
+eliminarX([P|R], X, [P|R1]):-
+	X > 1,
+	X1 is X - 1,
+	eliminarX(R, X1, R1).
+eliminarsegunX([], _Pos, []).
+eliminarsegunX([P|R], Pos, [Res1|Res2]):-
+	eliminarX(P, Pos, Res1),
+	eliminarsegunX(R, Pos, Res2).
